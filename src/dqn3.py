fixed softmax that did not make diff on actions

src/dqn3.py
@@ -21,7 +21,6 @@ class Dqn3(object):
         self.a_dim = (1,)
         self._gamma = 0.99
         self._lambda = float(args['--lambda'])
-        self.theta_act = float(args['--theta_act'])
         self.theta_learn = float(args['--theta_learn'])
         self.margin = float(args['--margin'])
         self.layers = [int(l) for l in args['--layers'].split(',')]
@@ -118,10 +117,10 @@ class Dqn3(object):
                              )(h)
         return Q_values
 
-    def act(self, exp):
+    def act(self, exp, theta=1):
         input = self.wrapper.make_input(exp)
         qvals = self.qvals(input)[0].squeeze()
-        probs = softmax(qvals, theta=self.theta_act)
+        probs = softmax(qvals, theta=theta)
         action = np.random.choice(range(qvals.shape[0]), p=probs)
         return np.expand_dims(action, axis=1), probs
 

src/envs/playroomReward.py
@@ -178,10 +178,11 @@ class PlayroomReward(Env):
             return self.go(dep[0], dep[1]), False
 
 if __name__ == '__main__':
-    env = PlayroomReward(multistart=True, sparsity=10)
+    env = PlayroomReward(multistart=True, sparsity=1)
     for task in range(25):
         s = env.reset()
         sumr = 0
+        i = 0
         while True:
             a, done = env.opt_action(task)
             if done:
@@ -190,7 +191,9 @@ if __name__ == '__main__':
                 a = np.expand_dims(a, axis=1)
                 s, r, t, _ = env.step(a)
                 sumr += r
+                i += 1
         print(sumr)
+        print(i)
     # s = env.reset()
     # step = 0
     # ep_step = 0

src/main.py
@@ -46,8 +46,7 @@ Options:
   --IS VAL                 [default: no]
   --targetClip VAL         [default: 0]
   --lambda VAL             [default: 0]
-  --theta_act VAL          [default: 1]
-  --theta_learn VAL        [default: 1]
+  --theta_learn VAL        [default: 10]
   --goal_replay VAL        [default: 0]
   --dueling VAL            [default: 0]
 """
@@ -133,7 +132,7 @@ if __name__ == '__main__':
 
     while env_step < int(args['--max_steps']):
 
-        a, probs = agent.act(exp)
+        a, probs = agent.act(exp, theta=max(0, 100*(env_step - 1e4) / 9e4))
         exp['a0'], exp['mu0'], exp['origin'] = a, probs[a], np.expand_dims(0, axis=1)
         state, r, term, info = env.step(a.squeeze())
         exp['s1'] = state.copy()

src/run_agent.py
@@ -1,27 +1,29 @@
 from keras.models import load_model
 import numpy as np
-from envs.rooms1 import Rooms1
+from envs.playroomReward import PlayroomReward
 from utils.util import softmax
 
-model = load_model('../log/local/3_Rooms1-v0/20190212150118_358700/log_steps/model')
+model = load_model('../log/cluster/0803/dqn_PlayroomRewardSparse1-v0/20190308130951_578259/model')
 
-env = Rooms1(args={'--tutoronly': '-1'})
+env = PlayroomReward(sparsity=1)
 
 s = env.reset()
-x = np.random.randint(env.nR)
-y = np.random.randint(env.nC)
-g = np.array(env.rescale([x,y]))
+# x = np.random.randint(env.nR)
+# y = np.random.randint(env.nC)
+# g = np.array(env.rescale([x,y]))
 i = 0
-print(g, '\n')
-while np.linalg.norm(s-g, axis=-1) > 0.001 and i < 500:
-    env.render(goal=(x,y))
-    input = [np.expand_dims(i, axis=0) for i in [s, g]]
+sum = 0
+while i < 200:
+    # print('state: ', env.x, env.y)
+    input = [np.expand_dims(i, axis=0) for i in [s, np.empty(0)]]
     qvals = model.predict(input)[0].squeeze()
-    print(qvals)
-    print(softmax(qvals, theta=2))
+    # print('qvals:',qvals)
+    print('probs: ', softmax(qvals, theta=2))
     # action = np.argmax(qvals, axis=0)
-    action = np.random.choice(range(qvals.shape[0]), p=softmax(qvals, theta=1))
+    action = np.random.choice(range(qvals.shape[0]), p=softmax(qvals, theta=10))
+    # print('action: ', action)
     a = np.expand_dims(action, axis=1)
-    s = env.step(a)[0]
+    s, r, _,_ = env.step(a)
+    sum += r
+    print('sum: ', sum)
     i += 1
-env.render(goal=(x,y))